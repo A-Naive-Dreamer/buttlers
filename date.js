236b61e815710b295923b337b839b27c14aa3dc3
A-Naive-Dreamer <40853350+A-Naive-Dreamer@users.noreply.github.com>
2018-08-20 04:10:51 +0700

    fix

--- date.js
+++ date.js
@@ -1,7 +1,5 @@
-var buttlers = function() {};
-
-buttlers.prototype.Date = function(days, months) {
-	this.days = [
+Buttlers.prototype.date = {
+	days: [
 		"Sunday",
 		"Monday",
 		"Tuesday",
@@ -9,8 +7,8 @@ buttlers.prototype.Date = function(days, months) {
 		"Thursday",
 		"Friday",
 		"Saturday"
-	];
-	this.months = [
+	],
+	months: [
 		"January",
 		"February",
 		"March",
@@ -23,100 +21,21 @@ buttlers.prototype.Date = function(days, months) {
 		"October",
 		"November",
 		"December"
-	];
-
-	if(!isNull(days)) {
-		setDays(days);
+	],
+	setMonths(months) {
+		if (Array.isArray(months) && months.length >= 12) {
+			this.months = months.slice(0, 12);
+		}
+	},
+	getMonths() {
+		return this.months;
+	},
+	setDays(days) {
+		if (Array.isArray(days) && days.length >= 12) {
+			this.days = days.slice(0, 8);
+		}
+	},
+	getDays() {
+		return Buttlers.prototype.date.days;
 	}
-
-	if(!isNull(months)) {
-		setMonths(months);
-	}
-}
-
-buttlers.Date.prototype.setMonths = function(months) {
-	if(typeof months === "string") {
-		months = months.split(" ");
-	}
-
-	if(months.length >= 12) {
-		months
-}
-
-function getMonths() {
-	return months;
-}
-
-function getFullMonth(months = getMonths, monthOrder) {
-	if(typeof months === "string") {
-		months.split(" ");
-	}
-
-	if(Array.isArray(months)) {
-		if(months.length === 12) {
-			if(monthOrder === 0) {
-				return months[0];
-			} else if(monthOrder === 1) {
-				return months[1];
-			} else if(monthOrder === 2) {
-				return months[2];
-			} else if(monthOrder === 3) {
-				return months[3];
-			} else if(monthOrder === 4) {
-				return months[4];
-			} else if(monthOrder === 5) {
-				return months[5];
-			} else if(monthOrder === 6) {
-				return months[6];
-			} else if(monthOrder === 7) {
-				return months[7];
-			} else if(monthOrder === 8) {
-				return months[8];
-			} else if(monthOrder === 9) {
-				return months[9];
-			} else if(monthOrder === 10) {
-				return months[10];
-			} else if(monthOrder === 11) {
-				return months[11];
-			}
-				case 2:
-					return months[2];
-				case 3:
-					return months[3];
-				case 4:
-					return months[3];
-				case 5:
-					return "June";
-        case 6:
-            return "July";
-        case 7:
-            return "August";
-        case 8:
-            return "September";
-        case 9:
-            return "October";
-        case 10:
-            return "November";
-        case 11:
-            return "December";
-    }
-}
-
-function getFullDays() {
-    switch(new Date().getDay()) {
-        case 0:
-            return "Sunday";
-        case 1:
-            return "Monday";
-        case 2:
-            return "Tuesday";
-        case 3:
-            return "Wednesday";
-        case 4:
-            return "Thursday";
-        case 5:
-            return "Friday";
-        case 6:
-            return "Saturday";
-    }
 }
--- math.js
+++ math.js
@@ -1,66 +1,84 @@
-function primeNumbers(start, end) {
-    if(start < end) {
-        let primeNumbers = [],
-			x = 0;
+Buttlers.prototype.math = {
+    primeNumbers(numbers, start = 0, end = 0) {
+        if (Array.isArray(numbers) && numbers.length > 0) {
+            return numbers.filter(this.isPrimeNumber);
+        } else {
+            start = parseInt(start);
+            end = parseInt(end);
 
-        while(start <= end) {
-            if(isPrimeNumber(start)) {
-                primeNumbers[x] = start;
-                ++x;
+            if (start < end) {
+                let primeNumbers = [],
+                    x = 0;
+
+                while (start <= end) {
+                    if (this.isPrimeNumber(start)) {
+                        primeNumbers[x] = start;
+                        ++x;
+                    }
+
+                    ++start;
+                }
+
+                if (x > 0) {
+                    return primeNumbers;
+                }
+            }
+        }
+
+        return false;
+    },
+    isPrimeNumber(number) {
+        number = parseInt(number);
+
+        if (number > 1) {
+            if (number < 4 || number === 5 || number === 7) {
+                return true;
             }
 
-            ++start;
+            if (number % 2 === 0 || number % 3 === 0 || number % 5 === 0 || number % 7 === 0) {
+                return false;
+            } else {
+                return true;
+            }
         }
 
-        if(x > 0) {
-            return primeNumbers;
+        return false;
+    },
+    naturalNumbers(numbers, start, end) {
+        if (Array.isArray(numbers) && numbers.length > 0) {
+            return numbers.filter(this.isNaturalNumber);
+        } else {
+            start = parseInt(start);
+            end = parseInt(end);
+
+            if (start < end) {
+                let naturalNumbers = [],
+                    x = 0;
+
+                while (start <= end) {
+                    if (this.isNaturalNumber(start)) {
+                        naturalNumbers[x] = start;
+                        ++x;
+                    }
+
+                    ++start;
+                }
+
+                if (x > 0) {
+                    return naturalNumbers;
+                }
+            }
         }
-    }
 
-    return false;
-}
+        return false;
+    },
+    isNaturalNumber(number) {
+        number = parseInt(number);
 
-function isPrimeNumber(number) {
-    if(number > 1) {
-        if(number < 4 || number === 5 || number === 7) {
+        if (number > 0) {
             return true;
         }
 
-        if(number % 2 !== 0 || number % 3 !== 0 || number % 5 !== 0 || number % 7 !== 0) {
-            return true;
-        }
+        return false;
     }
-
-    return false;
 }
-
-function naturalNumbers(start, end) {
-    if(start < end) {
-        let naturalNumbers= [],
-            x = 0;
-
-        while(start <= end) {
-            if(isNaturalNumber(start)) {
-                naturalNumbers[x] = start;
-                ++x;
-            }
-
-            ++start;
-        }
-
-        if(x > 0) {
-            return naturalNumbers;
-        }
-    }
-
-    return false;
-}
-
-function isNaturalNumber(number) {
-    if(number > 0) {
-        return true;
-    }
-
-    return false;
-}
-
